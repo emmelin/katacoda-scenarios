80b06bcdc529512a6f57c4cc73d22e76783b4604
Melin <emmanuel.melin@univ-orleans.fr>
2022-07-07 10:07:59 +0200

    t

--- managejson.py
+++ managejson.py
@@ -20,19 +20,23 @@ for n in ("intro","steps","finish","assets"):
     if n in df["details"]:
         details[n]=df["details"][n]
 
-new["details"]=details
+
 
 
 for step in details["steps"]:
-    print(step["text"]) 
-    namestep=step["text"].split("/")[-1].split(".")[0]
+    textinit=step["text"]
+    namestep=textinit.split("/")[-1].split(".")[0]
     filename=os.path.dirname(indexfile)+"/"+namestep
     if not os.path.exists(filename):
            os.makedirs(filename)
-           print(step["text"][1:])
-           os.rename(os.path.dirname(indexfile)+"/"+step["text"][1:],filename+"/"+namestep+".md")
+           print(textinit[1:])
+           os.rename(os.path.dirname(indexfile)+"/"+textinit[1:],filename+"/"+namestep+".md")
+    step["text"]=namestep+"/"+namestep+".md"
+    step["background"]="assets/cleanrep.sh"
 
 #print(new)
+new["details"]=details
+
 
 with open('result.json', 'w', encoding='utf8') as fp:
     json.dump(new, fp, indent=4, ensure_ascii=False)
